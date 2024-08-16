daa04ee4e5398b626aa0839dd86ff784e57dfe38
Jordan <iraldejordan10@gmail.com>
2024-08-15 21:49:59 -0300

    Update 32_calculoSistemas.py

--- Python/32_calculoSistemas.py
+++ Python/32_calculoSistemas.py
@@ -1,55 +1,108 @@
-print("Inicio")
-while True:
-    voltaje = int(input("Ingrese Volaje: "))
-    resistenciasSerie = []
-    resistenciasParalelos = []
+def CodigoMIO():
 
-    CantidadResistencias = int(input("Ingrese la cantidad de resistencias que se encuentran: "))
-    for i in range(CantidadResistencias):
-        serieParalelo = input("Su resistencia es en serie o paralelo: ")
-        if serieParalelo == 'serie':
-            resistencia = int(input(f"Resistencia {i+1}: \n"))
-            resistenciasSerie.append(resistencia)
-        elif serieParalelo == 'paralelo':
-            resistencia = int(input(f"Resistencia {i+1}: \n"))
-            calculoResistenciaparalelo = 1/resistencia
-            resistenciasParalelos.append(calculoResistenciaparalelo)
-            print("La resistencia total paralelo es", sum(resistenciasParalelos))
-        else:
-            print("mal ingresado")
-            exit()
-    if len(resistenciasParalelos) == 1:
-        print("Error, para ser paralelo deben haber 2 resistencias minimo")
+    print("Inicio")
+    resistencias = {}
 
-    resistenciaTotal = sum(resistenciasSerie) + sum(resistenciasParalelos)
-    print("Resistencia Total= ", resistenciaTotal)
+    while True:
+        voltaje = int(input("Ingrese Voltaje: "))
+        resistenciasSerie = []
+        resistenciasParalelos = []
 
-    IntensidadTotal = voltaje / resistenciaTotal
-    print("Intensidad Total= ", IntensidadTotal)
+        CantidadResistencias = int(input("Ingrese la cantidad de resistencias que se encuentran: "))
+        for i in range(CantidadResistencias):
 
+            serieParalelo = input("Su resistencia es en serie o paralelo: ")
 
+            if serieParalelo == 'serie':
+                resistencia = int(input(f"Resistencia {i+1}: \n"))
+                resistenciasSerie.append(resistencia)
+                resistencias[i] = resistencia
 
-resistenciaTotal = sum(resistencias)
+            elif serieParalelo == 'paralelo':
+                resistencia = int(input(f"Resistencia {i+1}: \n"))
+                calculoResistenciaparalelo = 1/resistencia
+                resistenciasParalelos.append(calculoResistenciaparalelo)
+                print("La resistencia total paralelo es", sum(resistenciasParalelos))
+            else:
+                print("mal ingresado")
+                exit()
 
-print("Resistencia Total: ", resistenciaTotal)
+        if len(resistenciasParalelos) == 1:
+            print("Error, para ser paralelo deben haber 2 resistencias minimo")
 
-'''
-ntensidadTotal = voltaje / calcularResistenciaTotal
+        RpT = 1/sum(resistenciasParalelos)
+        resistenciaTotal = sum(resistenciasSerie) + RpT
+        print("Resistencia Total= ", resistenciaTotal)
 
-print("Intensidad Total: ", intensidadTotal)
+        IntensidadTotal = voltaje / resistenciaTotal
+        print("Intensidad Total= ", IntensidadTotal)
+        
+        print(resistencias)
 
-voltaje1 = intensidadTotal * resistencia1
-voltaje23 = intensidadTotal * resistencia23
-voltaje4 = intensidadTotal * resistencia4
+    #Calcular Intensidad
+    #Calcular Voltaje
 
-print(f"Voltaje 1: {voltaje1}, Voltaje 2: {voltaje23}, Voltaje 3: {voltaje23}, Voltaje 4: {voltaje4}")
+    print("Fin")
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg  # Import correcto
+import numpy as np
 
-intensidad1 = voltaje1 / resistencia1
-intensidad2 = voltaje23 / resistencia2
-intensidad3 = voltaje23 / resistencia3
-intensidad4 = voltaje4 / resistencia4
+def actualizar_grafico(resistenciasSerie, resistenciasParalelos):
+    plt.clf()
+    if resistenciasSerie:
+        plt.plot([0, 1], [1, 1], color='blue')  # Línea para resistencias en serie
+        for i in range(len(resistenciasSerie)):
+            plt.text(0.5 + i, 1, f"R{i+1}={resistenciasSerie[i]}", color='blue')
 
-print(f"Intensidad 1: {intensidad1}, Intensidad 2: {intensidad2}, Intensidad 3: {intensidad3}, Intensidad 4: {intensidad4}")
-'''
+    if resistenciasParalelos:
+        plt.plot([0.5, 0.5], [0, 1], color='red')  # Ramas paralelas
+        for i in range(len(resistenciasParalelos)):
+            plt.text(0.5, 0.5 - i, f"Rp{i+1}={1/resistenciasParalelos[i]}", color='red')
 
-print("Fin")
+    plt.draw()
+
+def agregar_resistencia():
+    tipo = tipo_resistencia.get()
+    valor = float(entry_resistencia.get())
+    
+    if tipo == 'serie':
+        resistenciasSerie.append(valor)
+    elif tipo == 'paralelo':
+        resistenciasParalelos.append(1/valor)
+        
+    actualizar_grafico(resistenciasSerie, resistenciasParalelos)
+
+resistenciasSerie = []
+resistenciasParalelos = []
+
+root = tk.Tk()
+root.title("Circuito Resistencia")
+
+frame = tk.Frame(root)
+frame.pack()
+
+label_voltaje = tk.Label(frame, text="Voltaje:")
+label_voltaje.pack(side="left")
+entry_voltaje = tk.Entry(frame)
+entry_voltaje.pack(side="left")
+
+label_resistencia = tk.Label(frame, text="Resistencia:")
+label_resistencia.pack(side="left")
+entry_resistencia = tk.Entry(frame)
+entry_resistencia.pack(side="left")
+
+tipo_resistencia = tk.StringVar(value='serie')
+radio_serie = tk.Radiobutton(frame, text="Serie", variable=tipo_resistencia, value='serie')
+radio_paralelo = tk.Radiobutton(frame, text="Paralelo", variable=tipo_resistencia, value='paralelo')
+radio_serie.pack(side="left")
+radio_paralelo.pack(side="left")
+
+boton_agregar = tk.Button(frame, text="Agregar Resistencia", command=agregar_resistencia)
+boton_agregar.pack(side="left")
+
+fig, ax = plt.subplots()
+canvas = FigureCanvasTkAgg(fig, master=root)  # Uso correcto de FigureCanvasTkAgg
+canvas.get_tk_widget().pack()
+
+root.mainloop()
